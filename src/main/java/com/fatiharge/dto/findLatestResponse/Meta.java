4335211102b34ce947b5e03121b50bc234f0d90c
fatihcetin <fatihcetin@tamfinans.com.tr>
2024-12-19 18:04:45 +0300

    feat : add time annotation

--- src/main/java/com/fatiharge/dto/findLatestResponse/Meta.java
+++ src/main/java/com/fatiharge/dto/findLatestResponse/Meta.java
@@ -1,8 +1,15 @@
 package com.fatiharge.dto.findLatestResponse;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class Meta {
     public String baseCurrency;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd'T'HH:mm:ss",
+            timezone = "Europe/Istanbul"
+    )
     public Date createdDate;
 }
