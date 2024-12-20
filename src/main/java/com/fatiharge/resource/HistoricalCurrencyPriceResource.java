169fc6c76e4e92e197a37a2cd0773a2a273bd7d0
fatihcetin <fatihcetin@tamfinans.com.tr>
2024-12-20 17:10:04 +0300

    feat : add Schema for Date field.

--- src/main/java/com/fatiharge/resource/HistoricalCurrencyPriceResource.java
+++ src/main/java/com/fatiharge/resource/HistoricalCurrencyPriceResource.java
@@ -1,31 +1,31 @@
-package com.fatiharge.resource;
-
-import com.fatiharge.domain.HistoricalCurrencyPrice;
-import com.fatiharge.service.HistoricalCurrencyPriceService;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
-
-import java.util.List;
-
-
-@Path("historical-currency-price")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
-public class HistoricalCurrencyPriceResource {
-
-    @Inject
-    HistoricalCurrencyPriceService historicalCurrencyPriceService;
-
-    @GET
-    public List<HistoricalCurrencyPrice> getHistoricalPrices(
-            @QueryParam("baseCurrency") String baseCurrency,
-            @QueryParam("date") String date
-            // (e.g. 2024-12-09)
-    ) {
-        throw new UnsupportedOperationException();
-        //        Date parsedDate = new SimpleDateFormat("yyyy-MM-dd").parse(date);
-        //        return historicalCurrencyPriceService.findByBaseCurrencyAndDate(baseCurrency, parsedDate);
-    }
-
-}
+//package com.fatiharge.resource;
+//
+//import com.fatiharge.domain.HistoricalCurrencyPrice;
+//import com.fatiharge.service.HistoricalCurrencyPriceService;
+//import jakarta.inject.Inject;
+//import jakarta.ws.rs.*;
+//import jakarta.ws.rs.core.MediaType;
+//
+//import java.util.List;
+//
+//
+//@Path("historical-currency-price")
+//@Produces(MediaType.APPLICATION_JSON)
+//@Consumes(MediaType.APPLICATION_JSON)
+//public class HistoricalCurrencyPriceResource {
+//
+//    @Inject
+//    HistoricalCurrencyPriceService historicalCurrencyPriceService;
+//
+//    @GET
+//    public List<HistoricalCurrencyPrice> getHistoricalPrices(
+//            @QueryParam("baseCurrency") String baseCurrency,
+//            @QueryParam("date") String date
+//            // (e.g. 2024-12-09)
+//    ) {
+//        throw new UnsupportedOperationException();
+//        //        Date parsedDate = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+//        //        return historicalCurrencyPriceService.findByBaseCurrencyAndDate(baseCurrency, parsedDate);
+//    }
+//
+//}
